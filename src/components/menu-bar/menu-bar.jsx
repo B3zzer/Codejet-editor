e503062eae052613f32ffa6af1eb965e8ab5d6fb
B3zzer <theobezerralouzada@outlook.com>
2026-01-20 13:53:22 -0300

    Add Codejet AI button to menu bar

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -997,7 +997,7 @@ class MenuBar extends React.Component {
                                                 <button>
                                             <button
   className={styles.menuBarButton}
-  onClick={() => alert('Codejet AI em breve 🚀')}
+  onClick={() => alert('Codejet AI em breve')}
 >
   Codejet AI
 </button>
